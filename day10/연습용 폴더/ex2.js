181dc95e76882313efb5c7c87a8483212a0bba38
challenger1736 <challenger1736@gmail.com>
2023-12-07 20:09:25 +0900

    20231207T2009 COMMIT

---
+++ day10/연습용 폴더/ex2.js
@@ -0,0 +1,25 @@
+console.log('동해물과 백두산이\n마르고 닳도록'); /* 개발자 도구로는 안보임 */
+
+console.log('이름\t나이\t지역'); /* 재밌다! */
+
+console.log('가나다'+'라마'+'바사아'+"자차카타"+'파하'); /* ''과 ""구분 없이 잘 + 연결연산자 됨 */
+
+console.log('안녕하세요'[0]);
+console.log('안녕하세요'[1]);
+console.log('안녕하세요'[2]);
+console.log('안녕하세요'[3]);
+console.log('안녕하세요'[4]);
+
+console.log('안녕하세요'.length)
+console.log('13'.length)
+/* console.log(13.length) 이건 안됨 */
+
+console.log(5+3*2)
+console.log((5+3)*2)
+
+console.log(10%2)
+console.log(10%3)
+console.log(10%4)
+
+console.log(true)
+console.log(false)
