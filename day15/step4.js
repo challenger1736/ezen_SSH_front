ab216ef81f13ca8784a5fff35eb68559a5f8fbec
challenger1736 <challenger1736@gmail.com>
2023-12-15 13:33:18 +0900

    20231215t1333 commit

--- day15/step4.js
+++ day15/step4.js
@@ -24,7 +24,7 @@ const 항목배열 = [];
 const 금액배열 = [];
 
 
-function 등록(){
+function 등록(){            /* 등록 펑션 */
     /* 하나라도 안 입력했을떄 알러트 */
     let 날짜 = document.querySelector('#dateInput').value
     let 항목 = document.querySelector('#contentInput').value
@@ -44,7 +44,8 @@ function 등록(){
 }
 
 
-function 삭제(삭제인덱스){ 
+
+function 삭제(삭제인덱스){      /* 삭제 펑션 */
     console.log(`삭제기능 확인`);
     날짜배열.splice(삭제인덱스, 1);
     항목배열.splice(삭제인덱스, 1);
@@ -54,7 +55,10 @@ function 삭제(삭제인덱스){
 }
 
 
-function 출력(){
+
+
+
+function 출력(){            /* 출력 펑션 */
     let output = ``;
 
     let totalMoney = ``;
@@ -65,11 +69,11 @@ function 출력(){
     for( let i = 0 ; i < 날짜배열.length ; i++){
 
         output += `  <div class="divt2">
-        <div>${날짜배열[i]}</div>
-        <div>${항목배열[i]}</div>
-        <div id="c${i}">${천단위(i)}원</div>
-        <div onclick="삭제(${i})" class="bold">삭제</div>        
-    </div>`
+                        <div>${날짜배열[i]}</div>
+                        <div>${항목배열[i]}</div>
+                        <div id="">${천단위(i)}원</div>
+                        <div onclick="삭제(${i})" class="bold">삭제</div>        
+                    </div>`
 
 
      }
@@ -88,7 +92,7 @@ function 출력(){
     document.querySelector('#contentInput').value = ""
     document.querySelector('#spendInput').value = ""
 
-    document.querySelector('#totalMoney').innerHTML = `${totalMoney} 원`
+    document.querySelector('#totalMoney').innerHTML = `${천단위2(totalMoney)} 원`
 
 
     
@@ -96,19 +100,57 @@ function 출력(){
     
     
 }
-
+/////////////////////////////////////////////////////////////
 /* 그냥 일단 금액 천단위로 만들어주는 함수 만들어보기 */
 
 /* 금액배열[i] string 이므로 */
 
-function 천단위(i){
-    let 천단위배열 = '';
-    for(a = 금액배열[i].length-1 ; a>=0 ; a--){
-    if(a%3==0){ 천단위배열 +=`${금액배열[i]},`}
-    else{ 천단위배열 += 금액배열[i]}
-    }
+function 천단위(i){         /* 천단위 로직 */
+    let 천단위배열 = [];
+    for(let a = 금액배열[i].length-1 ; a>=0 ; a--){
+    if(a%3==0 && a != 0 ){ 천단위배열.push(`${금액배열[i][a]},`)}
+    else{ 천단위배열.push(`${금액배열[i][a]}`)}
+}
+/* 여기서 return 천단위배열 하면 3,,3,3,3 이런식으로 나옴 */
 
-    document.querySelector(`c${i}`).innerHTML = 천단위배열
+ /* 천단위배열의 각 자리수를 다 연결 연산자로 더하는 로직 */
+  let 천단위배열플러스 = ``;    /* +=이랑 push랑 자꾸 헷갈리는 경우가 많음 */
+
+    for(let a = 0 ; a < 천단위배열.length ; a ++){
+       천단위배열플러스 += 천단위배열[a]
+    }
+ 
+/* 천단위배열[0]+천단위배열[1]~~~~~천단위배열[천단위배열.length-1] */
+ 
+return 천단위배열플러스
+
+}
+
+
+
+///////////////////////////////////////////////
+//★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+/*★★★ 지역변수 test 자리에 totalmonery를 넣는 식으로 다른 펑션에 천단위2 펑션을 적용할 수 있다. */
+//★★★★★★★★★★★★ 헷 갈 림 ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+function 천단위2(test){         /* 천단위 합계 로직 */
+    let 천단위배열 = [];
+    let st = String(test)
+    for(let a = st.length-1 ; a>=0 ; a--){
+    if(a%3==0 && a != 0 ){ 천단위배열.push(`${st[a]},`)}
+    else{ 천단위배열.push(`${st[a]}`)}
+}
+ 
+ /* 천단위배열의 각 자리수를 다 연결 연산자로 더하는 로직 */
+  let 천단위배열플러스 = ``; /* +=이랑 push랑 자꾸 헷갈리는 경우가 많음 */
+
+    for(let a = 0 ; a < 천단위배열.length ; a ++){
+       천단위배열플러스 += 천단위배열[a]
+    }
+ 
+/* 천단위배열[0]+천단위배열[1]~~~~~천단위배열[천단위배열.length-1] */
+ 
+return 천단위배열플러스
+
 }
 
 
