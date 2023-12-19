43a111fbb31e1dba1f568654477d8abe5de27fde
challenger1736 <challenger1736@gmail.com>
2023-12-19 18:37:22 +0900

    20231219T1837 Commit

--- day11/연습용폴더/ex2.js
+++ day11/연습용폴더/ex2.js
@@ -1,6 +1,6 @@
 
 document.write('ddddd')
-document.body.innerHTML = "안녕 자바스크립트6";  // 이거 왜 안되는지
+document.body.innerHTML = "안녕 자바스크립트6"; 
     /* HTML안에 입력할 때는 innerHTML */
     /* HTML에서 불러올 때는 HTML로 이용할 수 있음 */
 /* 
